2caab5f0cabefd02ced548636218bdbd0de8f258
PHILIPPE Romain <romain.philippe78@free.fr>
2015-05-06 11:04:53 +0200

    nouvelle page d'accueil

---
+++ src/main/java/action/HomeAction.java
@@ -0,0 +1,43 @@
+package action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mapping.Category;
+import mapping.ObjectMuseum;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import dao.CategoryDaoImpl;
+
+public class HomeAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<Category> listCategory;
+	
+	public String getAllCategory(){
+		System.out.println("getAllCategory");
+		CategoryDaoImpl categoriesDao = new CategoryDaoImpl();
+		
+		listCategory =new ArrayList<Category>( categoriesDao.getCategory());
+		System.out.println("getAllCategory -- listCategory.size :"+listCategory.size());
+		for(Category e:listCategory){
+			System.out.println(e.getName_f());
+		}
+		System.out.println("getAllCategory -- listCategory.size :"+listCategory.get(0).getName_e());
+		return SUCCESS;
+	}
+
+	public List<Category> getListCategory() {
+		return listCategory;
+	}
+
+	public void setListCategory(List<Category> listCategory) {
+		this.listCategory = listCategory;
+	}
+	
+
+}
