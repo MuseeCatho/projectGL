94d68d62cad7ffe6cfb9e428742580452e37008f
PHILIPPE Romain <romain.philippe78@free.fr>
2015-05-30 19:10:18 +0200

    debut langues

--- src/main/java/action/LocaleAction.java
+++ src/main/java/action/LocaleAction.java
@@ -1,14 +1,33 @@
 package action;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import mapping.Category;
+
 import com.opensymphony.xwork2.ActionSupport;
+
+import dao.CategoryDaoImpl;
 
 public class LocaleAction extends ActionSupport{
 
+	private List<Category> listCategory;
+	
+	
 	//business logic
 	public String execute() {
-	
 		return "SUCCESS";
 
+	}
+	
+
+
+	public List<Category> getListCategory() {
+		return listCategory;
+	}
+
+	public void setListCategory(List<Category> listCategory) {
+		this.listCategory = listCategory;
 	}
 
 }
